e827a7df98869cd718242ec0ec9aae58938b69b2
Katiuha <ecaterina.muntean23@gmail.com>
2025-04-13 13:17:51 +0300

    Initial commit

---
+++ mounts/site/index.php
@@ -0,0 +1,8 @@
+<?php
+echo "Привет мир!<br>";
+echo "Hello, World with echo!<br>";
+print "Hello, World with print!<br>";
+$days = 288;
+echo "Конкатенация: " . $days . " дней<br>" ;
+$message = "Все возвращаются на работу!";
+echo "Двойные кавычки: $message<br>";
